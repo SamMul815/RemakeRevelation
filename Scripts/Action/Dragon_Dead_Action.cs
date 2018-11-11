2bf40b8dd37b31e3bb8d238ce3943376fe47bdae
JeonMinSu <session_min@naver.com>
2018-11-11 22:40:37 +0900

    2018-11-11(GameClear UI ---> 대충 생성해놓음) 내일 중으로 쉐이더 한번 넣어보고 해보겠음

--- Scripts/Action/Dragon_Dead_Action.cs
+++ Scripts/Action/Dragon_Dead_Action.cs
@@ -5,10 +5,12 @@ using DragonController;
 
 public class Dragon_Dead_Action : ActionTask
  {
+    bool isCrateUI;
 
     public override void Init()
     {
         base.Init();
+        isCrateUI = false;
     }
 
     public override void OnStart()
@@ -20,6 +22,12 @@ public class Dragon_Dead_Action : ActionTask
 
     public override bool Run()
     {
+        if (!isCrateUI)
+        {
+            Vector3 pos = DragonTransform.position;
+            UIManager.Instance.CreateGameClear(DragonTransform.position, -DragonTransform.forward, out isCrateUI);
+        }
+
         return false;
     }
 
